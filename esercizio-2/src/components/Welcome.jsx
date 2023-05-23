8d2a87a6efec1f180642f3465248ffd43322beb9
zeno <giuseppezeno97@gmail.com>
2023-05-23 15:22:57 +0200

    esercizio 2 completato

--- esercizio-2/src/components/Welcome.jsx
+++ esercizio-2/src/components/Welcome.jsx
@@ -7,3 +7,14 @@
 // }
 
 // export default Welcome;
+
+import React from "react";
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <p>Ciao: {this.props.name}</p>
+        )
+    }
+}
+
