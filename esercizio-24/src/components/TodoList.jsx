064d6be4d1b39380f7f784179cfed537043b861b
zeno <giuseppezeno97@gmail.com>
2023-06-03 19:31:42 +0200

    esercizio 24 - list

---
+++ esercizio-24/src/components/TodoList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+
+    state = {
+        arr: this.props.fruits,
+        inputValue: ""
+    }
+
+    handleInput = (event) => {
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+    handleOnClick = () => {
+        this.setState({
+            arr: [...this.props.fruits, this.state.inputValue ],
+            inputValue: "" })
+    }
+
+    render() {
+        return (
+            <>
+            <div>
+                <ul>
+                    {this.state.arr.map((element, index) => {
+                      return <li key={index}>{element}</li>
+                    })}
+                </ul>
+                <input onChange={this.handleInput} type="text" value={this.state.inputValue}/>
+                <button onClick={this.handleOnClick}>Add fruits</button>
+            </div>
+            </>
+        )
+    }
+}
