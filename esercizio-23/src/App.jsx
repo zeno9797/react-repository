0d7f8b8542ee4eb511be63fcfc6f80657988251d
zeno <giuseppezeno97@gmail.com>
2023-06-03 17:22:38 +0200

    esercizio 23 form list

---
+++ esercizio-23/src/App.jsx
@@ -0,0 +1,12 @@
+import { TodoList } from "./components/TodoList"
+
+function App() {
+
+  return (
+    <>
+     <TodoList fruits = {["apple", "banana", "orange", "mango", "grape", "kiwi", "pineapple", "strawberry", "watermelon"]} />
+    </>
+  )
+}
+
+export default App
