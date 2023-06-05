da5c159d512e8032daf6e8b7987e69a3c00cdbfe
zeno <giuseppezeno97@gmail.com>
2023-06-05 19:33:47 +0200

    esercizio 28

---
+++ esercizio-28/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export class App extends React.Component {
+  
+    render() {
+      return (
+        <>
+        
+        </>
+      )
+    }
+}
+
+export default App;
+
+
